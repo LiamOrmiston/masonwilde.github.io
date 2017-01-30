5b195b4ab23b05814674c332e8d16ffa8bbeeb71
Mason Wilde <masonwilde@ku.edu>
2017-01-29 20:14:41 -0600

    Fixed homework1.js

--- homework1.js
+++ homework1.js
@@ -8,7 +8,7 @@ drawBox = function(var size)
 	for(var i = 0; i < size; i++){
 		for(var j = 0; j < size; j++)
 		{
-			if( i==0 || i==(size-) || j==0 || j==(size-1))
+			if( i==0 || i==(size-1) || j==0 || j==(size-1))
 			{
 				chalk.print('*');
 			}
