697123a62141cf5b91ba3da956f27310fc288d88
Mason Wilde <masonwilde@ku.edu>
2017-04-20 14:30:07 -0500

    functionality of reading document and printing list

--- Overwatch/ow.js
+++ Overwatch/ow.js
@@ -16,20 +16,34 @@ oReq.send();
 function printStuff(string){
 	console.log(string);
 	var list = string.split('\n');
+	var newList = [];
+	list.forEach((x) => {
+		if(x != ""){
+			newList.push(x);
+		}
+	})
+	console.log(newList);
 	var sortedList = [];
 	var p1 = []
-	for (var name in list){
-		var string = "<p>" + list[name] + "</p>\n";
+	for (var name in newList){
+		//var string = "<p>" + newList[name] + "</p>\n";
 		//$("#table").append(string);
-		p1.push($.get(
-			"https://overwatch-api-ku.herokuapp.com/profile/pc/us/" + list[name],
+		p1.push(
+			$.get(
+			"https://overwatch-api-ku.herokuapp.com/profile/pc/us/" + newList[name],
 	  	 	function(data) {sortedList.push({name: data.username, rank: data.competitive.rank});},
-			"json"))
+			"json")
+		)
+		console.log(sortedList)
 	}
-	Promise.all(p1).then(finish);
-	function finish() {
-		sortedList.sort(function(a,b){return a.rank - b.rank});
-		console.log(sortedList);
-		sortedList.map(function(data) {$("#table").append("<p>" + data.name + " : " + data.rank + "</p>")})
-	}
+	//console.log(sortedList);
+	Promise.all(p1).then(() => {console.log(sortedList); finish(sortedList);});
+
+}
+
+function finish(sortedList) {
+	console.log(sortedList);
+	sortedList.sort(function(a,b){return b.rank - a.rank});
+	console.log(sortedList);
+	sortedList.map(function(data) {$("#table").append("<p>" + data.name + " : " + data.rank + "</p>")})
 }
